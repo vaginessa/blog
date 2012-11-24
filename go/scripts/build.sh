14258af15f2e7ec9a205653d57a1f47ea8ef087d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-11-23 19:27:15 -0800

    fix syntax error

--- go/scripts/build.sh
+++ go/scripts/build.sh
@@ -3,7 +3,7 @@ go build -o blog_app *.go
 if [ "$?" -ne 0 ]; then echo "failed to build"; exit 1; fi
 cp util.go extract_crashing_lines.go tools/importappengine
 # only exists locally, not on the server
-if [-e tools/importappengine ]
+if [ -e tools/importappengine ]; then
 	cd tools/importappengine
 	go build -o importappeng *.go
 	if [ "$?" -ne 0 ]; then echo "failed to build"; exit 1; fi
