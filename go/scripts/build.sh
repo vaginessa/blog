3e6a908acbb3595e0607ee6e5f937c3330c7cd0d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-11-24 17:15:30 -0800

    only build import tool locally

--- go/scripts/build.sh
+++ go/scripts/build.sh
@@ -1,9 +1,9 @@
 export GOPATH=`pwd`/ext:$GOPATH
 go build -o blog_app *.go
 if [ "$?" -ne 0 ]; then echo "failed to build"; exit 1; fi
-cp util.go extract_crashing_lines.go tools/importappengine
 # only exists locally, not on the server
 if [ -e tools/importappengine ]; then
+	cp util.go extract_crashing_lines.go tools/importappengine
 	cd tools/importappengine
 	go build -o importappeng *.go
 	if [ "$?" -ne 0 ]; then echo "failed to build"; exit 1; fi
