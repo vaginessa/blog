a43a6708c9b239ea9d596b4d28367635eab8e928
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-07-03 23:18:15 -0700

    update prettify js library

--- main.go
+++ main.go
@@ -230,12 +230,12 @@ func jQueryUrl() string {
 func prettifyJsUrl() string {
 	//return "//cdnjs.cloudflare.com/ajax/libs/prettify/188.0.0/prettify.js"
 	//return "/js/prettify.js"
-	return "//cdnjs.cloudflare.com/ajax/libs/prettify/r224/prettify.js"
+	return "//cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.js"
 }
 
 func prettifyCssUrl() string {
 	//return "/js/prettify.css"
-	return "//cdnjs.cloudflare.com/ajax/libs/prettify/r224/prettify.css"
+	return "//cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.css"
 }
 
 func makeTimingHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
