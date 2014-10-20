e39cdc18a3ffd829182463ab0279037385885481
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-10-19 20:06:11 -0700

    start filtering out 2.5.2 crashes

--- handler_crashes.go
+++ handler_crashes.go
@@ -19,7 +19,7 @@ import (
 
 var blacklistedSumatraVersions = []string{"1.5.1", "1.6", "1.7", "1.8", "1.9",
 	"2.0", "2.0.1", "2.1", "2.1.1", "2.2", "2.2.1", "2.3", "2.3.1", "2.3.2",
-	"2.4", "2.5", "2.5.1", "2.5 dbg", "2.5.1 dbg"}
+	"2.4", "2.5", "2.5.1", "2.5 dbg", "2.5.1 dbg", "2.5.2"}
 
 func (c *Crash) Version() string {
 	ver := *c.ProgramVersion
