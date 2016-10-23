6b5ef9736d6f552900e47479c2144a86af5660ba
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-10-23 00:25:14 -0700

    rename files

--- scripts/rename-blog-posts.js
+++ scripts/rename-blog-posts.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
-const walk    = require('walk');
+const child_process = require('child_process');
+const fs = require('fs');
+const walk = require('walk');
+
 const files   = [];
 
 function clean_path(path) {
@@ -18,22 +21,68 @@ walker.on('file', function(root, stat, next) {
   next();
 });
 
+walker.on('end', rename_files);
+
 // YYYY-DD/foo.txt -> YYYY/DD-foo.txt
 function path_rename(path) {
   let parts = path.split("/");
   const yyyy_dd = parts[0];
   const name = parts[1];
   parts = yyyy_dd.split("-");
+  if (parts.length !== 2) {
+    return null;
+  }
   const yyyy = parts[0];
   const dd = parts[1];
   return `${yyyy}/${dd}-${name}`;
 }
 
-walker.on('end', function() {
-  files.sort();
-  for (const path of files) {
-    const new_path = path_rename(path);
-    console.log(`${path} => ${new_path}`);
-    // TODO: run git rename
+function dir_for_path(path) {
+  const parts = path.split("/");
+  return parts[0];
+}
+
+function rename_git_files(to_rename) {
+  const paths = to_rename.pop();
+  if (!paths) {
+    return;
   }
-});
+  const [path, new_path] = paths;
+  const cmd = `git mv ${path} ${new_path}`;
+  console.log(`cmd: ${cmd}`);
+  child_process.exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error}, stdout: ${stdout}, stderr: ${stderr}`);
+      return;
+    }
+    rename_git_files(to_rename);
+  });
+}
+
+function mkdirIfNotExistsSync(dir) {
+    if (!fs.existsSync(dir)) {
+      console.log(`created ${dir}`);
+      fs.mkdirSync(dir);
+    }
+}
+
+function rename_files() {
+  files.sort();
+  const dirsDict = {};
+  const to_rename = [];
+  files.forEach(path => {
+    const new_path = path_rename(path);
+    if (new_path) {
+      const dir = dir_for_path(new_path);
+      dirsDict[dir] = 1;
+      to_rename.push([path, new_path]);
+    }
+  });
+
+  const dirs = Object.keys(dirsDict);
+  dirs.forEach(mkdirIfNotExistsSync);
+
+  rename_git_files(to_rename);
+}
+
+
