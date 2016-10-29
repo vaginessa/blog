41b28858e728d14e9093c79de6f57d612fa04c2d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-10-28 23:09:24 -0700

    add scripts/hyper_update.sh

---
+++ scripts/hyper_update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+hyper fip detach blog
+hyper stop blog
+hyper rm blog
+hyper run --size=s2 --restart=unless-stopped -d -p 80 -v blog:/data --name blog kjksf/blog:latest
+hyper fip attach 209.177.93.141 blog
