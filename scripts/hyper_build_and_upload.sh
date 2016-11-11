17e7a5129975a98248eb52069d5f3c892ad57da3
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-11-11 02:00:27 -0800

    upload to hyper via s3

--- scripts/hyper_build_and_upload.sh
+++ scripts/hyper_build_and_upload.sh
@@ -5,7 +5,7 @@ set -o errexit
 set -o pipefail
 
 GOOS=linux GOARCH=amd64 go build -o blog_app_linux
-
-docker build --tag kjksf/blog:latest --tag blog:latest .
-docker push kjksf/blog:latest
-hyper pull kjksf/blog:latest
+docker build --tag blog:latest .
+docker save blog:latest | gzip | aws s3 cp - s3://kjkpub/tmp/blog.tar.gz
+hyper load -i $(aws s3 presign s3://kjkpub/tmp/blog.tar.gz)
+aws s3 rm s3://kjkpub/tmp/blog.tar.gz
--- scripts/hyper_update.sh
+++ scripts/hyper_update.sh
@@ -7,5 +7,5 @@ set -o pipefail
 hyper fip detach blog
 hyper stop blog
 hyper rm blog
-hyper run --size=s2 --restart=unless-stopped -d -p 80 -v blog:/data --name blog kjksf/blog:latest
+hyper run --size=s2 --restart=unless-stopped -d -p 80 -v blog:/data --name blog blog:latest
 hyper fip attach 209.177.93.141 blog
