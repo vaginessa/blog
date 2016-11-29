11887f0053749d84fa20200420408328d6f793c2
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-11-29 02:08:25 -0800

    move verbose huper_build_and_upload.sh

--- scripts/hyper_build_and_upload.sh
+++ scripts/hyper_build_and_upload.sh
@@ -5,7 +5,10 @@ set -o errexit
 set -o pipefail
 
 GOOS=linux GOARCH=amd64 go build -o blog_app_linux
+echo "docker build"
 docker build --tag blog:latest .
+echo "docker save"
 docker save blog:latest | gzip | aws s3 cp - s3://kjkpub/tmp/blog.tar.gz
+echo "hyper load"
 hyper load -i $(aws s3 presign s3://kjkpub/tmp/blog.tar.gz)
 aws s3 rm s3://kjkpub/tmp/blog.tar.gz
