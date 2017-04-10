865af006622c1fa6ee043b5234ebe577c65dee08
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-04-09 22:05:03 -0700

    less docker tagging

--- scripts/docker_run_local.sh
+++ scripts/docker_run_local.sh
@@ -3,5 +3,5 @@ set -u -e -o pipefail
 
 GOOS=linux GOARCH=amd64 go build -o blog_app_linux
 
-docker build --tag kjksf/blog:latest --tag blog:latest .
-docker run --rm -it -v ~/data/blog:/data -p 5020:80 kjksf/blog:latest
+docker build --tag blog:latest .
+docker run --rm -it -v ~/data/blog:/data -p 5020:80 blog:latest
