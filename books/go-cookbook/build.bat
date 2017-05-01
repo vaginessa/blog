fbb451f8770ad03cb9d54dcb96f43f2b6edd1008
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-05-01 00:39:55 -0700

    remove obsolete files

--- books/go-cookbook/build.bat
+++
@@ -1,9 +0,0 @@
-@mkdir bin
-
-8g -o bin\build.8 src\build.go
-@if ERRORLEVEL 1 EXIT /B 1
-
-8l -o bin\build.exe bin\build.8
-@if ERRORLEVEL 1 EXIT /B 1
-
-bin\build.exe
--- scripts/regen.py
+++ scripts/regen.py
@@ -6,8 +6,50 @@ Re-generate html files from markdown (.md) files.
 pip install markdown web.py
 """
 
-import os, markdown, web, util
-from util import read_file_utf8, write_file_utf8, list_files, ext
+import os
+import codecs
+import markdown
+import web
+
+def read_file_utf8(path):
+	with codecs.open(path, "r", "utf8") as fo:
+		s = fo.read()
+	return s
+
+def write_file_utf8(path, s):
+	with codecs.open(path, "w", "utf8") as fo:
+		s = fo.write(s)
+
+def ext(path):
+    return os.path.splitext(path)[1].lower()
+
+# returns full paths of files in a given directory, potentially recursively,
+# potentially filtering file names by filter_func (which takes file path as
+# an argument)
+def list_files_g(d, filter_func=None, recur=False):
+    to_visit = [d]
+    while len(to_visit) > 0:
+        d = to_visit.pop(0)
+        for f in os.listdir(d):
+            path = os.path.join(d, f)
+            isdir = os.path.isdir(path)
+            if isdir:
+                if recur:
+                    to_visit.append(path)
+            else:
+                if filter_func is not None:
+                    if filter_func(path):
+                        yield path
+                else:
+                    yield path
+
+# generator => array
+def list_files(d, filter_func=None, recur=False):
+    return [path for path in list_files_g(d, filter_func, recur)]
+
+def delete_file(path):
+    if os.path.exists(path):
+        os.remove(path)
 
 def is_markdown_file(path):
 	return ext(path) in [".md"]
