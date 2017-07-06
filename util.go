f59a26efe453e5a81155e99ac0e955b719a83410
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-07-06 02:21:07 -0700

    fix urlify() once and for all

--- util.go
+++ util.go
@@ -4,21 +4,57 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"regexp"
 	"strings"
 )
 
-var (
-	patWs            = regexp.MustCompile(`\s+`)
-	patCharsToRemove = regexp.MustCompile("[+:*%&/(),]")
-)
+// whitelisted characters valid in url
+func validateRune(c rune) byte {
+	if c >= 'a' && c <= 'z' {
+		return byte(c)
+	}
+	if c >= '0' && c <= '9' {
+		return byte(c)
+	}
+	if c == '-' || c == '_' || c == '.' {
+		return byte(c)
+	}
+	if c == ' ' {
+		return '-'
+	}
+	return 0
+}
 
-// urlify generates safe url from tile
+func charCanRepeat(c byte) bool {
+	if c >= 'a' && c <= 'z' {
+		return true
+	}
+	if c >= '0' && c <= '9' {
+		return true
+	}
+	return false
+}
+
+// urlify generates safe url from tile by removing hazardous characters
 func urlify(title string) string {
 	s := strings.TrimSpace(title)
 	s = strings.ToLower(s)
-	s = patCharsToRemove.ReplaceAllString(s, "")
-	s = patWs.ReplaceAllString(s, "-")
+	var res []byte
+	for _, r := range s {
+		c := validateRune(r)
+		if c == 0 {
+			continue
+		}
+		// eliminute duplicate consequitive characters
+		var prev byte
+		if len(res) > 0 {
+			prev = res[len(res)-1]
+		}
+		if c == prev && !charCanRepeat(c) {
+			continue
+		}
+		res = append(res, c)
+	}
+	s = string(res)
 	if len(s) > 128 {
 		s = s[:128]
 	}
