bdc619f00506d20a9620f98116bef1fa63358ea5
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-07-06 22:43:13 -0700

    move sitemap.xml handler to handler_sitemap.go

---
+++ handler_sitemap.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/xml"
+	"net/http"
+	"path"
+	"time"
+)
+
+// SiteMapURLSet represents <urlset>
+type SiteMapURLSet struct {
+	XMLName xml.Name `xml:"urlset"`
+	Ns      string   `xml:"xmlns,attr"`
+	URLS    []SiteMapURL
+}
+
+func makeSiteMapURLSet() *SiteMapURLSet {
+	return &SiteMapURLSet{
+		Ns: "http://www.sitemaps.org/schemas/sitemap/0.9",
+	}
+}
+
+// SiteMapURL represents a single url
+type SiteMapURL struct {
+	XMLName      xml.Name `xml:"url"`
+	URL          string   `xml:"loc"`
+	LastModified string   `xml:"lastmod"`
+}
+
+// There are more static pages, but those are the important ones
+var staticURLS = []string{
+	"/book/go-cookbook.html",
+	"/articles/cbz-cbr-comic-book-reader-viewer-for-windows.html",
+	"/articles/chm-reader-viewer-for-windows.html",
+	"/articles/mobi-ebook-reader-viewer-for-windows.html",
+	"/articles/epub-ebook-reader-viewer-for-windows.html",
+	"/articles/where-to-get-free-ebooks-epub-mobi.html",
+	"/software/",
+	"static/documents.html",
+	"/dailynotes",
+}
+
+// /sitemap.xml
+func handleSiteMap(w http.ResponseWriter, r *http.Request) {
+	articles := getCachedArticles()
+	urlset := makeSiteMapURLSet()
+	var urls []SiteMapURL
+	for _, article := range articles {
+		pageURL := "https://" + path.Join(r.Host, article.Permalink())
+		uri := SiteMapURL{
+			URL:          pageURL,
+			LastModified: article.UpdatedOn.Format("2006-01-02"),
+		}
+		urls = append(urls, uri)
+	}
+
+	now := time.Now()
+	for _, staticURL := range staticURLS {
+		pageURL := "https://" + path.Join(r.Host, staticURL)
+		uri := SiteMapURL{
+			URL:          pageURL,
+			LastModified: now.Format("2006-01-02"),
+		}
+		urls = append(urls, uri)
+	}
+
+	for _, note := range notesAllNotes {
+		pageURL := "https://" + path.Join(r.Host, note.URL)
+		uri := SiteMapURL{
+			URL:          pageURL,
+			LastModified: note.Day.Format("2006-01-02"),
+		}
+		urls = append(urls, uri)
+	}
+
+	urlset.URLS = urls
+
+	xmlData, err := xml.MarshalIndent(urlset, " ", " ")
+	if err != nil {
+		serve404(w, r)
+		return
+	}
+	d := append([]byte(xml.Header), xmlData...)
+	serveXML(w, string(d))
+}
