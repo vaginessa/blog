461f174f365bb3059c78e618aa7a713a73ec2acf
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-12-07 14:39:00 -0800

    port s/newarticle.sh to s/newarticle.ps1

--- s/newarticle.ps1
+++ s/newarticle.ps1
@@ -3,11 +3,11 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 function exitIfFailed { if ($LASTEXITCODE -ne 0) { exit } }
 
-$title = $args[0]
-if ($title -eq "") {
+if ($args.Length -ne 1) {
     Write-Host "usage: ./s/newarticle.ps1 <title>"
     exit 1
 }
+$title = $args[0]
 
 $exe = ".\blog_app.exe"
 $plat = $PSVersionTable["Platform"]
