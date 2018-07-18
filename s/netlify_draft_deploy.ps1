4ff6a52ed51dea97eb678f7d79a9502f4084a68d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-07-17 23:13:05 -0700

    add s/netlify_draft_deploy.ps1

---
+++ s/netlify_draft_deploy.ps1
@@ -0,0 +1,12 @@
+#!/usr/bin/env pwsh
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+function exitIfFailed { if ($LASTEXITCODE -ne 0) { exit } }
+
+$exe = "./blog_app.exe" # name that works both on unix and win
+go build -o blog_app.exe
+exitIfFailed
+Start-Process -Wait -FilePath $exe
+Remove-Item -Path $exe
+
+netlifyctl deploy --draft
