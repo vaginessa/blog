a3d0b0e14dd5529c2a2ecd900337797f434fe813
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-07-24 00:07:58 -0700

    trying to add cron in travis

--- s/travis_build.sh
+++ s/travis_build.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 set -u -e -o pipefail -o verbose
 
-go build -o blog
-./blog -deploy
-./netlifyctl -A $NETLIFY_TOKEN deploy || true
-cat netlifyctl-debug.log || true
+build()
+{
+    echo "building"
+    go build -o blog
+    ./blog -deploy
+    ./netlifyctl -A $NETLIFY_TOKEN deploy || true
+    cat netlifyctl-debug.log || true
+}
+
+setup_git()
+{
+  git config --global user.email "kkowalczyk@gmail.com"
+  git config --global user.name "Krzysztof Kowalczyk"
+  git config --global github.user "kjk"
+  git config --global github.token "${GH_TOKEN}"
+}
+
+update_from_notion()
+{
+    echo "cron: updating from notion"
+    setup_git()
+
+    go build -o blog
+    ./blog -redownload-notion
+    git commit -am "travis: update from notion"
+    git push
+}
+
+if [ "${TRAVIS_EVENT_TYPE}" == "cron"]; then
+    update_from_notio
+else
+    build
+fi
