5c933f5dc822607adbbeff2755b1128477284e17
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-04-09 14:38:04 -0700

    improve toggle blocks; handle links to pages

--- notion_to_html.go
+++ notion_to_html.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"html"
+	"html/template"
+	"strings"
+
 	"github.com/kjk/notionapi"
 	"github.com/kjk/notionapi/tohtml"
 )
@@ -13,18 +18,6 @@ type ImageMapping struct {
 }
 
 /*
-// HTMLGenerator generates an .html file for single notion page
-type HTMLGenerator struct {
-	f            *bytes.Buffer
-	page         *notionapi.Page
-	notionClient *notionapi.Client
-
-	level    int
-	levelCls string
-	nToggle  int
-	err      error
-}
-
 // only hex chars seem to be valid
 func isValidNotionIDChar(c byte) bool {
 	switch {
@@ -91,49 +84,8 @@ func (g *HTMLGenerator) maybeReplaceNotionLink(uri string) string {
 	return article.URL()
 }
 
-func (g *HTMLGenerator) getURLAndTitleForBlock(block *notionapi.Block) (string, string) {
-	id := normalizeID(block.ID)
-	article := g.idToArticle(id)
-	if article == nil {
-		title := block.Title
-		lg("No article for id %s %s\n", id, title)
-		url := "/article/" + id + "/" + urlify(title)
-		return url, title
-	}
-
-	return article.URL(), article.Title
-}
-
-// "date_format": "relative",
-// "start_date": "2019-03-26",
-// "type": "date"
-func formatDate(d *notionapi.Date) string {
-	if d.DateFormat == "relative" {
-		return d.StartDate
-	}
-	lg("formatDate: unhandled date:\n")
-	pretty.Print(d)
-	return "@TODO: date"
-}
-
 func (g *HTMLGenerator) genInlineBlock(b *notionapi.InlineBlock) {
 	var start, close string
-	if b.AttrFlags&notionapi.AttrBold != 0 {
-		start += "<b>"
-		close += "</b>"
-	}
-	if b.AttrFlags&notionapi.AttrItalic != 0 {
-		start += "<i>"
-		close += "</i>"
-	}
-	if b.AttrFlags&notionapi.AttrStrikeThrought != 0 {
-		start += "<strike>"
-		close += "</strike>"
-	}
-	if b.AttrFlags&notionapi.AttrCode != 0 {
-		start += "<code>"
-		close += "</code>"
-	}
 	skipText := false
 	if b.Link != "" {
 		link := g.maybeReplaceNotionLink(b.Link)
@@ -144,201 +96,12 @@ func (g *HTMLGenerator) genInlineBlock(b *notionapi.InlineBlock) {
 		start += fmt.Sprintf(`<span class="user">@%s</span>`, b.UserID)
 		skipText = true
 	}
-	if b.Date != nil {
-		s := formatDate(b.Date)
-		start += fmt.Sprintf(`<span class="date">%s</span>`, s)
-		skipText = true
-	}
-	if !skipText {
-		start += b.Text
-	}
-	g.writeString(start + close)
-}
-
-// v is expected to be
-// [
-// 	[
-// 		"foo"
-// 	]
-// ]
-// and we want to return "foo"
-// If not present or unexpected shape, return ""
-// is still visible
-func propsValueToText(v interface{}) string {
-	if v == nil {
-		return ""
-	}
-
-	// [ [ "foo" ]]
-	a, ok := v.([]interface{})
-	if !ok {
-		return fmt.Sprintf("type1: %T", v)
-	}
-	// [ "foo" ]
-	if len(a) == 0 {
-		return ""
-	}
-	v = a[0]
-	a, ok = v.([]interface{})
-	if !ok {
-		return fmt.Sprintf("type2: %T", v)
-	}
-	// "foo"
-	if len(a) == 0 {
-		return ""
-	}
-	v = a[0]
-	str, ok := v.(string)
-	if !ok {
-		return fmt.Sprintf("type3: %T", v)
-	}
-	return str
-}
-
-func (g *HTMLGenerator) genVideo(block *notionapi.Block) {
-	f := block.FormatVideo
-	s := fmt.Sprintf(`<iframe width="%d" height="%d" src="%s" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>`, f.BlockWidth, f.BlockHeight, f.DisplaySource)
-	g.writeString(s)
-}
-
-func (g *HTMLGenerator) genCollectionView(block *notionapi.Block) {
-	viewInfo := block.CollectionViews[0]
-	view := viewInfo.CollectionView
-	columns := view.Format.TableProperties
-	s := `<table class="notion-table"><thead><tr>`
-	for _, col := range columns {
-		colName := col.Property
-		colInfo := viewInfo.Collection.CollectionSchema[colName]
-		name := colInfo.Name
-		s += `<th>` + html.EscapeString(name) + `</th>`
-	}
-	s += `</tr></thead>`
-	s += `<tbody>`
-	for _, row := range viewInfo.CollectionRows {
-		s += `<tr>`
-		props := row.Properties
-		for _, col := range columns {
-			colName := col.Property
-			v := props[colName]
-			colVal := propsValueToText(v)
-			if colVal == "" {
-				// use &nbsp; so that empty row still shows up
-				// could also set a min-height to 1em or sth. like that
-				s += `<td>&nbsp;</td>`
-			} else {
-				//colInfo := viewInfo.Collection.CollectionSchema[colName]
-				// TODO: format colVal according to colInfo
-				s += `<td>` + html.EscapeString(colVal) + `</td>`
-			}
-		}
-		s += `</tr>`
-	}
-	s += `</tbody>`
-	s += `</table>`
-	g.writeString(s)
-}
-
-// Children of BlockColumnList are BlockColumn blocks
-func (g *HTMLGenerator) genColumnList(block *notionapi.Block) {
-	panicIf(block.Type != notionapi.BlockColumnList, "unexpected block type '%s'", block.Type)
-	nColumns := len(block.Content)
-	panicIf(nColumns == 0, "has no columns")
-	// TODO: for now equal width columns
-	s := `<div class="column-list">`
-	g.writeString(s)
-
-	for _, col := range block.Content {
-		// TODO: get column ration from col.FormatColumn.ColumnRation, which is float 0...1
-		panicIf(col.Type != notionapi.BlockColumn, "unexpected block type '%s'", col.Type)
-		g.writeString(`<div>`)
-		g.genBlocks(col.Content)
-		g.writeString(`</div>`)
-	}
-
-	s = `</div>`
-	g.writeString(s)
-}
-
-func (g *HTMLGenerator) genToggle(block *notionapi.Block) {
-	panicIf(block.Type != notionapi.BlockToggle, "unexpected block type '%s'", block.Type)
-	g.nToggle++
-	id := strconv.Itoa(g.nToggle)
-
-	inline := g.getInline(block.InlineContent)
-
-	b := g.newBuffer()
-	g.genBlocks(block.Content)
-	inner := g.restoreBuffer(b)
-
-	s := fmt.Sprintf(`<div style="width: 100%%; margin-top: 2px; margin-bottom: 1px;">
-    <div style="display: flex; align-items: flex-start; width: 100%%; padding-left: 2px; color: rgb(66, 66, 65);">
-
-        <div style="margin-right: 4px; width: 24px; flex-grow: 0; flex-shrink: 0; display: flex; align-items: center; justify-content: center; min-height: calc((1.5em + 3px) + 3px); padding-right: 2px;">
-            <div id="toggle-toggle-%s" onclick="javascript:onToggleClick(this)" class="toggler" style="align-items: center; user-select: none; display: flex; width: 1.25rem; height: 1.25rem; justify-content: center; flex-shrink: 0;">
-
-                <svg id="toggle-closer-%s" width="100%%" height="100%%" viewBox="0 0 100 100" style="fill: currentcolor; display: none; width: 0.6875em; height: 0.6875em; transition: transform 300ms ease-in-out; transform: rotateZ(180deg);">
-                    <polygon points="5.9,88.2 50,11.8 94.1,88.2 "></polygon>
-                </svg>
-
-                <svg id="toggle-opener-%s" width="100%%" height="100%%" viewBox="0 0 100 100" style="fill: currentcolor; display: block; width: 0.6875em; height: 0.6875em; transition: transform 300ms ease-in-out; transform: rotateZ(90deg);">
-                    <polygon points="5.9,88.2 50,11.8 94.1,88.2 "></polygon>
-                </svg>
-            </div>
-        </div>
-
-        <div style="flex: 1 1 0px; min-width: 1px;">
-            <div style="display: flex;">
-                <div style="padding-top: 3px; padding-bottom: 3px">%s</div>
-            </div>
-
-            <div style="margin-left: -2px; display: none" id="toggle-content-%s">
-                <div style="display: flex; flex-direction: column;">
-                    <div style="width: 100%%; margin-top: 2px; margin-bottom: 0px;">
-                        <div style="color: rgb(66, 66, 65);">
-							<div style="">
-								%s
-                                <!-- <div style="padding: 3px 2px;">text inside list</div> -->
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-`, id, id, id, string(inline), id, string(inner))
-	g.writeString(s)
 }
 
 func (g *HTMLGenerator) genBlock(block *notionapi.Block) {
-	g.levelCls = ""
-	if g.level > 0 {
-		g.levelCls = fmt.Sprintf(" lvl%d", g.level)
-	}
-
 	switch block.Type {
-	case notionapi.BlockToggle:
-		g.genToggle(block)
-	case notionapi.BlockPage:
-		cls := "page"
-		if block.IsLinkToPage() {
-			cls = "page-link"
-		}
-		url, title := g.getURLAndTitleForBlock(block)
-		title = template.HTMLEscapeString(title)
-		html := fmt.Sprintf(`<div class="%s%s"><a href="%s">%s</a></div>`, cls, g.levelCls, url, title)
-		fmt.Fprintf(g.f, "%s\n", html)
 	case notionapi.BlockImage:
 		g.genImage(block)
-	case notionapi.BlockColumnList:
-		g.genColumnList(block)
-	case notionapi.BlockCollectionView:
-		g.genCollectionView(block)
-	case notionapi.BlockVideo:
-		g.genVideo(block)
-	case notionapi.BlockFile:
-		// TODO: add support for this type in notionapi, render as a link
-		// block id: 8c5fd467-989b-4180-902c-9b5d30c6568d
 	default:
 		lg("Unsupported block type '%s', id: %s\n", block.Type, block.ID)
 		panic(fmt.Sprintf("Unsupported block type '%s'", block.Type))
@@ -360,7 +123,6 @@ func (g *HTMLGenerator) genImage(block *notionapi.Block) {
 	g.images = append(g.images, im)
 	fmt.Fprintf(g.f, `<img class="blog-img" src="%s" />`+"\n", relURL)
 }
-
 */
 
 // HTMLRenderer keeps data
@@ -373,6 +135,19 @@ type HTMLRenderer struct {
 	r *tohtml.HTMLRenderer
 }
 
+func (r *HTMLRenderer) getURLAndTitleForBlock(block *notionapi.Block) (string, string) {
+	id := notionapi.ToNoDashID(block.ID)
+	article := r.idToArticle(id)
+	if article == nil {
+		title := block.Title
+		lg("No article for id %s %s\n", id, title)
+		url := "/article/" + id + "/" + urlify(title)
+		return url, title
+	}
+
+	return article.URL(), article.Title
+}
+
 // RenderPage renders BlockPage
 func (r *HTMLRenderer) RenderPage(block *notionapi.Block, entering bool) bool {
 	tp := block.GetPageType()
@@ -383,7 +158,22 @@ func (r *HTMLRenderer) RenderPage(block *notionapi.Block, entering bool) bool {
 		return true
 	}
 
-	return false
+	var cls string
+	if tp == notionapi.BlockPageSubPage {
+		cls = "page"
+	} else if tp == notionapi.BlockPageLink {
+		cls = "page-link"
+	} else {
+		panic("unexpected page type")
+	}
+
+	url, title := r.getURLAndTitleForBlock(block)
+	title = html.EscapeString(title)
+	content := fmt.Sprintf(`<a href="%s">%s</a>`, url, title)
+	attrs := []string{"class", cls}
+	title = template.HTMLEscapeString(title)
+	r.r.WriteElement(block, "div", attrs, content, entering)
+	return true
 }
 
 // RenderCode renders BlockCode
@@ -399,12 +189,76 @@ func (r *HTMLRenderer) RenderCode(block *notionapi.Block, entering bool) bool {
 	return true
 }
 
+var (
+	toggleEntering = `
+<div style="width: 100%%; margin-top: 2px; margin-bottom: 1px;">
+    <div style="display: flex; align-items: flex-start; width: 100%%; padding-left: 2px; color: rgb(66, 66, 65);">
+
+        <div style="margin-right: 4px; width: 24px; flex-grow: 0; flex-shrink: 0; display: flex; align-items: center; justify-content: center; min-height: calc((1.5em + 3px) + 3px); padding-right: 2px;">
+            <div id="toggle-toggle-{{id}}" onclick="javascript:onToggleClick(this)" class="toggler" style="align-items: center; user-select: none; display: flex; width: 1.25rem; height: 1.25rem; justify-content: center; flex-shrink: 0;">
+
+                <svg id="toggle-closer-{{id}}" width="100%%" height="100%%" viewBox="0 0 100 100" style="fill: currentcolor; display: none; width: 0.6875em; height: 0.6875em; transition: transform 300ms ease-in-out; transform: rotateZ(180deg);">
+                    <polygon points="5.9,88.2 50,11.8 94.1,88.2 "></polygon>
+                </svg>
+
+                <svg id="toggle-opener-{{id}}" width="100%%" height="100%%" viewBox="0 0 100 100" style="fill: currentcolor; display: block; width: 0.6875em; height: 0.6875em; transition: transform 300ms ease-in-out; transform: rotateZ(90deg);">
+                    <polygon points="5.9,88.2 50,11.8 94.1,88.2 "></polygon>
+                </svg>
+            </div>
+        </div>
+
+        <div style="flex: 1 1 0px; min-width: 1px;">
+            <div style="display: flex;">
+                <div style="padding-top: 3px; padding-bottom: 3px">{{inline}}</div>
+            </div>
+
+            <div style="margin-left: -2px; display: none" id="toggle-content-{{id}}">
+                <div style="display: flex; flex-direction: column;">
+                    <div style="width: 100%%; margin-top: 2px; margin-bottom: 0px;">
+                        <div style="color: rgb(66, 66, 65);">
+							<div style="">
+`
+	toggleClosing = `
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+`
+)
+
+// RenderToggle renders BlockToggle blocks
+func (r *HTMLRenderer) RenderToggle(block *notionapi.Block, entering bool) bool {
+	panicIf(block.Type != notionapi.BlockToggle, "unexpected block type '%s'", block.Type)
+
+	if entering {
+		// TODO: could do it without pushing buffers
+		r.r.PushNewBuffer()
+		r.r.RenderInlines(block.InlineContent)
+		inline := r.r.PopBuffer().String()
+		id := notionapi.ToNoDashID(block.ID)
+		s := strings.Replace(toggleEntering, "{{id}}", id, -1)
+		s = strings.Replace(s, "{{inline}}", inline, -1)
+		r.r.WriteString(s)
+
+	} else {
+		r.r.WriteString(toggleClosing)
+	}
+	// we handled it
+	return true
+}
+
 func (r *HTMLRenderer) blockRenderOverride(block *notionapi.Block, entering bool) bool {
 	switch block.Type {
 	case notionapi.BlockPage:
 		return r.RenderPage(block, entering)
 	case notionapi.BlockCode:
 		return r.RenderCode(block, entering)
+	case notionapi.BlockToggle:
+		return r.RenderToggle(block, entering)
 	}
 	return false
 }
@@ -417,6 +271,8 @@ func NewHTMLRenderer(c *notionapi.Client, page *notionapi.Page) *HTMLRenderer {
 	}
 
 	r := tohtml.NewHTMLRenderer(page)
+	r.PanicOnFailures = true
+	r.AddIDAttribute = true
 	r.Data = res
 	r.RenderBlockOverride = res.blockRenderOverride
 
