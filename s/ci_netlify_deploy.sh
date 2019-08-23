f63a00d9c6893a641d106d5fa36fa3c8bd10a620
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-08-23 13:56:47 -0700

    split netlifyctl download into a separate step

--- s/ci_netlify_deploy.sh
+++ s/ci_netlify_deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -u -e -o pipefail
 
-wget https://github.com/netlify/netlifyctl/releases/download/v0.4.0/netlifyctl-linux-amd64-0.4.0.tar.gz
-tar -xvf netlifyctl-linux-amd64-0.4.0.tar.gz
+set
 
 go build -o blog
 ./blog -deploy
-./netlifyctl -A $NETLIFY_TOKEN deploy || true
+./netlifyctl -A ${NETLIFY_TOKEN} deploy || true
 cat netlifyctl-debug.log || true
