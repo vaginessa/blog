b51fdbf2d6cabff1e0d8eebab8edd506d49644f9
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-09-04 21:13:28 -0700

    auto-detect an extension for image/jpeg

--- go.mod
+++ go.mod
@@ -22,3 +22,5 @@ require (
 	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
+
+go 1.13
--- notion_import.go
+++ notion_import.go
@@ -44,9 +44,13 @@ func guessExt(fileName string, contentType string) string {
 	case ".png", ".jpg", ".jpeg":
 		return ext
 	}
+
+	contentType = strings.ToLower(contentType)
 	switch contentType {
 	case "image/png":
 		return ".png"
+	case "image/jpeg":
+		return ".jpg"
 	}
 	panic(fmt.Errorf("didn't find ext for file '%s', content type '%s'", fileName, contentType))
 }
